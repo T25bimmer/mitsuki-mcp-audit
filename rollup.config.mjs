ac11f44507367b7b91eab8bb905856c590ecc7cc
百濑美月 <15230787+MomoseMitsuki@user.noreply.gitee.com>
2025-09-28 00:01:51 +0800

    build: first commit

---
+++ rollup.config.mjs
@@ -0,0 +1,32 @@
+import { glob } from 'glob';
+import typescript from '@rollup/plugin-typescript';
+
+const files = await glob('src/**/*.ts');
+
+export default {
+  input: files,
+
+  output: {
+    dir: 'dist',
+    format: 'es',
+    preserveModules: true,
+    preserveModulesRoot: 'src',
+    entryFileNames: '[name].js',
+  },
+
+  external: [
+    'axios',
+    'spark-md5',
+  ],
+
+  plugins: [
+    typescript({
+      tsconfig: './tsconfig.json',
+      declaration: false,
+      declarationMap: false
+    })
+  ],
+
+  treeshake: true
+};
+
