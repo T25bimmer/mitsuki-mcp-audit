cffa9503eb3b743693dda9382c2b13c096ecc352
百濑美月 <15230787+MomoseMitsuki@user.noreply.gitee.com>
2025-09-28 00:39:07 +0800

    build: create the workplace to input the cache

---
+++ src/workDir/index.ts
@@ -0,0 +1,16 @@
+import fs from "fs"
+
+import { join } from "path"
+import { uniqueId,getDirname } from "../common/util"
+
+const __dirname = getDirname(import.meta.url)
+
+const basePath = join(__dirname,'../..')
+const workBasePath = join(basePath,"work")
+fs.mkdirSync(workBasePath, { recursive: true })
+
+export async function createWorkDir() {
+    const workDir = join(workBasePath,uniqueId())
+    await fs.promises.mkdir(workDir, { recursive:true })
+    return workDir
+}
