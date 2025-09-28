3cf44697ee1d9980fd77ddc5b0ef2b89d91624b5
百濑美月 <15230787+MomoseMitsuki@user.noreply.gitee.com>
2025-09-28 19:39:20 +0800

    build: parse the root

--- src/parseProject/index.ts
+++ src/parseProject/index.ts
@@ -1,7 +1,8 @@
 import { parseLocalProject } from "./parseLocalProject"
 import { parseRemoteProject } from "./parseRemoteProject"
+import type { PackageJson } from "../@types"
 
-export function parseProject(projectRoot:string) {
+export function parseProject(projectRoot:string):Promise<PackageJson> {
     if (projectRoot.startsWith('http://') || projectRoot.startsWith('https://')) {
         return parseRemoteProject(projectRoot)
     }
